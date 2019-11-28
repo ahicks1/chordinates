9eae19f14b1864c11fe17c3489840da68ac7c2f3
ahicks1 <ahicks1@trinity.edu>
2019-11-27 17:10:40 -0700

    API development work

--- APIChordsFunction/index.js
+++ APIChordsFunction/index.js
@@ -8,6 +8,37 @@ const pool = new pg.Pool({
     connectionTimeoutMillis: 10000
 });
 
+const getQueryText = `SELECT "pinID", "whenMade", "longitude", "latitude", 
+"permission", c."sID", c."tID", t."name" as timename, "start", "end", "url", 
+s."name" as songname, "artist", "genre" 
+FROM public."Chord" as c, public."Time" as t, public."Song" as s 
+WHERE t."tID" = c."tID" AND s."sID" = c."sID" AND "uID" = $1;`;
+
+const dummyUUID = '103c946d-ef98-42c1-b0be-cc815818c405';
+
+const getChordsForUser = async (client, userID = dummyUUID) => {
+  const query = {
+    text:getQueryText,
+    values: [userID],
+  }
+  const response = await client.query(query);
+  return response.rows;
+}
+
+const addChordForUser = async (client, longitude, latitude, permission, timeId, songURL) => {
+  const query = {
+    text:getQueryText,
+    values: [userID],
+  }
+  const response = await client.query(query);
+  return response.rows;
+}
+
+const closeAndReturn = (client, response) => {
+  //client.release(true);
+  return response;
+}
+
 exports.handler = async (event, context) => {
 
   // https://github.com/brianc/node-postgres/issues/930#issuecomment-230362178
@@ -16,20 +47,47 @@ exports.handler = async (event, context) => {
   const client = await pool.connect();
   console.log('DB Connected')
 
-  
-  //const client = await pool.connect();
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify('Hello from Lambda!'),
-  };
+  console.log(event);
+  console.log(context);
+
+  const userID = event.requestContext.identity.cognitoIdentityId;
+
+  //TODO: Paginate!
   try {
-    const r = await client.query('SELECT * FROM public."Song"');
-    console.log(r);
-    response.body = JSON.stringify(r);
+    switch (event.httpMethod) {
+      case 'GET':
+        const rows = await getChordsForUser(client, userID);
+        return closeAndReturn(client, {
+          statusCode: 200,
+          body: JSON.stringify({chords: rows})
+        });
+      // case 'POST':
+      //   break;
+      default:
+          return closeAndReturn(client, {
+            statusCode: 405,
+            body: `${event.resource} doesn't support method ${event.httpMethod}`
+          });
+    }
   } finally {
-    // https://github.com/brianc/node-postgres/issues/1180#issuecomment-270589769
     client.release(true);
   }
   
-  return response;
+
+  
+  // //const client = await pool.connect();
+  // const response = {
+  //   statusCode: 200,
+  //   body: JSON.stringify('Hello from Lambda!'),
+  // };
+  // try {
+  //   const r = await client.query('SELECT * FROM public."Song"');
+  //   console.log(r);
+  //   response.body = JSON.stringify(r);
+  // } finally {
+  //   // https://github.com/brianc/node-postgres/issues/1180#issuecomment-270589769
+  //   client.release(true);
+  // }
+  
+  // return response;
 };
---
+++ APISpotifyMiddleman/index.js
@@ -0,0 +1,86 @@
+var aws = require('aws-sdk');
+const fetch = require('node-fetch');
+var lambda = new aws.Lambda({
+  region: 'us-east-2' //change to your region
+});
+
+const SPOTIFY_CLIENT_ID = process.env.SPOTIFY_CLIENT_ID;
+const SPOTIFY_CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET;
+const SPOTIFY_AUTH_B64 = process.env.SPOTIFY_AUTH_B64;
+/***
+ * 
+ * This function calls the spotify API to get song information for the given url
+ * 
+ * */
+let accessToken;
+const getAccessToken = async () => {
+  if(accessToken) return accessToken;
+  const response = await fetch('https://accounts.spotify.com/api/token', {
+    method: 'POST',
+    headers: {
+      Authorization: `Basic ${SPOTIFY_AUTH_B64}`,
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: 'grant_type=client_credentials',
+  })
+  const json = await response.json();
+  console.log(json);
+  accessToken = json.access_token;
+  return accessToken;
+}
+
+const getSongInformation = async (songID) => {
+  const token = await getAccessToken();
+  const response = await fetch(`https://api.spotify.com/v1/tracks/${songID}`, {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${token}`
+    },
+  })
+  const body = await response.json();
+  return {
+    artist: body.artists[0].name,
+    title: body.name,
+    album: body.album.name,
+    id: songID,
+  }
+}
+/**
+ * 
+ * @param {String} lambdaName 
+ * @param {String} payload 
+ */
+const invokeLambdaPromise = (lambdaName, payload) => {
+  return new Promise((resolve, reject) => {
+    lambda.invoke({
+      FunctionName: lambdaName,
+      Payload: payload // pass params
+    }, function(error, data) {
+      if (error) {
+        reject(error);
+      }
+      if(data.Payload){
+       resolve(data.Payload);
+      }
+    });
+  })
+}
+
+const badRequest = () => {
+  const response = {
+    statusCode: 400,
+    body: JSON.stringify('Malformed request'),
+  };
+  return response;
+}
+
+
+exports.handler = async (event) => {
+  const idMatch = JSON.parse(event.body).url.match(/track\/([\w]+)/);
+  if (idMatch == null) return badRequest();
+  const songID = idMatch[1];
+  const songInfo = await getSongInformation(songID)
+
+  const lambdaPayload = JSON.stringify({...event, songInfo: songInfo});
+  return await invokeLambdaPromise('APIChordsFunction', lambdaPayload);
+};
